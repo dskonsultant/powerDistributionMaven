09d4f78bf1797fdd57117cb8662f84902eeee4d6
shrey <shrey@192.168.1.3>
2016-03-12 16:18:30 +0530

    Adding the BaseDTO

---
+++ CommonServiceLayer/src/main/java/com/dskonsultants/common/base/dto/BaseDTO.java
@@ -0,0 +1,18 @@
+package com.dskonsultants.common.base.dto;
+
+import java.io.Serializable;
+
+/**
+ * Abstract class to be implemented by all the DTO's
+ * 
+ * @version 1.0
+ * @author Shrey Khandelwal
+ */
+public abstract class BaseDTO implements Serializable {
+
+	/**
+	 * serialVersionUID
+	 */
+	private static final long serialVersionUID = 4120107535693212835L;
+
+}
