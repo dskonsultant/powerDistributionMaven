22edbe4ce9ccb74b93d3c8c9324cc0fc005b7477
Digi <Digi@192.168.1.6>
2016-03-13 19:26:41 +0530

    Update

--- BusinessServiceLayer/src/main/java/com/urjamitra/email/service/EmailService.java
+++ BusinessServiceLayer/src/main/java/com/urjamitra/email/service/EmailService.java
@@ -3,7 +3,7 @@ package com.urjamitra.email.service;
 import com.dskonsultants.common.exception.GenericException;
 import com.urjamitra.email.dto.EmailDTO;
 
-public interface MailService {
+public interface EmailService {
 
 	public boolean sendMail(EmailDTO emailDTO) throws GenericException; // MailDTO
 
--- BusinessServiceLayer/src/main/java/com/urjamitra/email/service/impl/MailServiceImpl.java
+++ BusinessServiceLayer/src/main/java/com/urjamitra/email/service/impl/MailServiceImpl.java
@@ -1,9 +1,9 @@
 package com.urjamitra.email.service.impl;
 
 import com.urjamitra.email.dto.EmailDTO;
-import com.urjamitra.email.service.MailService;
+import com.urjamitra.email.service.EmailService;
 
-public class MailServiceImpl implements MailService {
+public class MailServiceImpl implements EmailService {
 
 	@Override
 	public boolean sendMail(EmailDTO emailDTO) {
