73cb69d2959393744a19986b2d012235aa960118
Digi <Digi@192.168.1.6>
2016-03-13 20:08:02 +0530

    Update

--- BusinessServiceLayer/src/main/java/com/urjamitra/role/dto/RoleDTO.java
+++ BusinessServiceLayer/src/main/java/com/urjamitra/role/dto/RoleDTO.java
@@ -1,7 +1,29 @@
 package com.urjamitra.role.dto;
 
+import java.util.List;
+import java.util.Map;
+
 import com.dskonsultants.common.base.dto.BaseDTO;
 
-public class RoleDTO extends BaseDTO{
+public class RoleDTO extends BaseDTO {
+
+	private String userType;
+	private Map<String, List<String>> accessRights;
+
+	public String getUserType() {
+		return userType;
+	}
+
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+
+	public Map<String, List<String>> getAccessRights() {
+		return accessRights;
+	}
+
+	public void setAccessRights(Map<String, List<String>> accessRights) {
+		this.accessRights = accessRights;
+	}
 
 }
