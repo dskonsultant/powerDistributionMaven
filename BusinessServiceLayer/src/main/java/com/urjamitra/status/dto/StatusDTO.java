ad71dfdfbc953a9667d1e448becce8b28d4c2112
Digi <Digi@192.168.1.5>
2016-03-16 23:38:53 +0530

    Update

--- BusinessServiceLayer/src/main/java/com/urjamitra/status/dto/StatusDTO.java
+++ BusinessServiceLayer/src/main/java/com/urjamitra/status/dto/StatusDTO.java
@@ -2,9 +2,25 @@ package com.urjamitra.status.dto;
 
 import com.dskonsultants.common.base.dto.BaseDTO;
 
-
-public class StatusDTO extends BaseDTO{
-
+public class StatusDTO extends BaseDTO {
+	
 	private long statusId;
 	private String statusName;
+
+	public long getStatusId() {
+		return statusId;
+	}
+
+	public void setStatusId(long statusId) {
+		this.statusId = statusId;
+	}
+
+	public String getStatusName() {
+		return statusName;
+	}
+
+	public void setStatusName(String statusName) {
+		this.statusName = statusName;
+	}
+
 }
